b20899164cc0dd7c8924c7475a7db4550a461115
park-sm <nambawon1@naver.com>
2022-09-07 16:44:04 +0900

    finish

--- section19/01-starting-project/src/store/cart-slice.js
+++ section19/01-starting-project/src/store/cart-slice.js
@@ -4,14 +4,19 @@ const cartSlice = createSlice({
   name: "cart",
   initialState: {
     items: [],
-    totalQuantity: 0, //수량의 합
+    totalQuantity: 0,
+    changed: false,
   },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
-      // dispatch될 떄 추가정보를 dispatch해야하기 때문에 actions이 필요하다
       const newItem = action.payload;
-      const existingItem = state.items.find((item) => item.id === newItem.id); // 이미 항목이 존재하는지 파악
+      const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
+      state.changed = true;
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
@@ -19,9 +24,8 @@ const cartSlice = createSlice({
           quantity: 1,
           totalPrice: newItem.price,
           name: newItem.title,
-        }); //리더스 툴킷이 자동으로 내부적으로 조작하지 않게 해서 push 사용 가능
+        });
       } else {
-        //항목이 존재하는 경우
         existingItem.quantity++;
         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
@@ -30,11 +34,10 @@ const cartSlice = createSlice({
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
       state.totalQuantity--;
+      state.changed = true;
       if (existingItem.quantity === 1) {
-        //1과 같으면 배열에서 항목을 완전히 제거한다.
-        state.items = state.itmes.filter((item) => item.id !== id);
+        state.items = state.items.filter((item) => item.id !== id);
       } else {
-        //1보다 크면 단지 항목의 숫자를 하나 줄이면 된다.
         existingItem.quantity--;
         existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
       }
