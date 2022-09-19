5f6db402b36e1f9a1d1c209d8ca1fa51df058441
park-sm <nambawon1@naver.com>
2022-09-20 00:16:19 +0900

    section20-add command

--- section20/09-time-to-practice-starting-code/src/pages/QuoteDetail.js
+++ section20/09-time-to-practice-starting-code/src/pages/QuoteDetail.js
@@ -1,21 +1,18 @@
 import { Fragment, useEffect } from "react";
 import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
+
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
 import useHttp from "../hooks/use-http";
 import { getSingleQuote } from "../lib/api";
 import LoadingSpinner from "../components/UI/LoadingSpinner";
 
-const DUMMY_QUOTES = [
-  { id: "q1", author: "Max", text: "Learning React is fun!" },
-  { id: "q2", author: "Maximilian", text: "Learning React is great!" },
-];
-
 const QuoteDetail = () => {
   const match = useRouteMatch();
   const params = useParams();
 
   const { quoteId } = params;
+
   const {
     sendRequest,
     status,
@@ -38,6 +35,7 @@ const QuoteDetail = () => {
   if (error) {
     return <p className="centered">{error}</p>;
   }
+
   if (!loadedQuote.text) {
     return <p>No quote found!</p>;
   }
