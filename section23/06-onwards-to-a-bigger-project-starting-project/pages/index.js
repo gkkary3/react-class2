2cbcac1160611063086361c53e684324467edc5d
park-sm <nambawon1@naver.com>
2022-09-27 08:59:54 +0900

    api 디렉터리 추가

--- section23/06-onwards-to-a-bigger-project-starting-project/pages/index.js
+++ section23/06-onwards-to-a-bigger-project-starting-project/pages/index.js
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
@@ -6,30 +5,45 @@ const DUMMY_MEETUPS = [
     id: "m1",
     title: "A First Meetup",
     image:
-      "https://upload.wikimedia.org/wikipedia/commons/1/12/View_of_saint_Peter_basilica_from_a_roof.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
     address: "Some address 5, 12345 Some City",
-    description: "this is a first meetup!",
+    description: "This is a first meetup!",
   },
   {
     id: "m2",
     title: "A Second Meetup",
     image:
-      "https://upload.wikimedia.org/wikipedia/commons/1/12/View_of_saint_Peter_basilica_from_a_roof.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
     address: "Some address 10, 12345 Some City",
-    description: "this is a Second meetup!",
+    description: "This is a second meetup!",
   },
 ];
 
-function HomePage() {
-  const [loadedMeetups, setLoadedMeetups] = useState([]); //상태관리하기 위해 사용
+function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
+}
 
-  useEffect(() => {
-    // Http 요청을 보내기 위해 사용
-    // send a http request and fetch data
-    setLoadedMeetups(DUMMY_MEETUPS);
-  }, []);
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
 
-  return <MeetupList meetups={loadedMeetups} />;
+//   // fetch data from an API
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS
+//     }
+//   };
+// }
+
+export async function getStaticProps() {
+  // fetch data from an API
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+    revalidate: 1,
+  };
 }
 
 export default HomePage;
